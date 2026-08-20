258d6d8189c07cd382aa849e7248e5b291a0cc04
draglide <darkzzangelzz@gmail.com>
2026-08-20 17:39:27 +0700

    feat(server): add Spring Boot entry point

---
+++ apps/server/src/main/java/io/wyrmgate/iam/WyrmgateIamApplication.java
@@ -0,0 +1,12 @@
+package io.wyrmgate.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyrmgateIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WyrmgateIamApplication.class, args);
+    }
+}
