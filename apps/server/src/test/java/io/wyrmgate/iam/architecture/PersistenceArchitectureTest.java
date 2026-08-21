b3d02eb9fce17d1fa0ede7dd5ec640c0cb8288f2
draglide <darkzzangelzz@gmail.com>
2026-08-21 16:07:51 +0700

    test: enforce persistence framework boundaries

---
+++ apps/server/src/test/java/io/wyrmgate/iam/architecture/PersistenceArchitectureTest.java
@@ -0,0 +1,20 @@
+package io.wyrmgate.iam.architecture;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "io.wyrmgate.iam", importOptions = ImportOption.DoNotIncludeTests.class)
+class PersistenceArchitectureTest {
+
+    @ArchTest
+    static final ArchRule frameworkNeutralDomainAndContextBoundaries = noClasses()
+            .that()
+            .resideInAnyPackage("..domain..", "..platform.id..", "..platform.tenant..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("org.springframework..", "java.sql..", "javax.sql..");
+}
