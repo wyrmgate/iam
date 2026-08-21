99636e211155480b5af0f168f2fb4c78889187dd
draglide <darkzzangelzz@gmail.com>
2026-08-21 17:02:32 +0700

    test: advance persistence bootstrap checkpoint to V7

--- apps/server/src/test/java/io/wyrmgate/iam/platform/persistence/PersistenceFoundationIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/platform/persistence/PersistenceFoundationIntegrationTest.java
@@ -63,7 +63,7 @@ class PersistenceFoundationIntegrationTest {
         scheduledWork = new JdbcScheduledWorkRepository(jdbc, idGenerator);
         transactions = new SpringTransactionExecutor(new DataSourceTransactionManager(dataSource));
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("6");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("7");
     }
 
     @AfterAll
