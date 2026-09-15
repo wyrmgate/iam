2ec90ff1334b1693729830fdc344e05c0c13224f
draglide <darkzzangelzz@gmail.com>
2026-09-15 16:24:21 +0700

    tests: expect Administration migration

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/IdentityPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/IdentityPersistenceIntegrationTest.java
@@ -64,7 +64,7 @@ class IdentityPersistenceIntegrationTest {
                 idGenerator,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("8");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("9");
     }
 
     @AfterAll
