00794e7da74d2d8d2ce048b2251170ecb5ead52b
draglide <darkzzangelzz@gmail.com>
2026-09-21 17:39:16 +0700

    test: advance Flyway baseline to v17

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/SourceCorrelationPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/SourceCorrelationPersistenceIntegrationTest.java
@@ -76,7 +76,7 @@ class SourceCorrelationPersistenceIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("16");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("17");
     }
 
     @AfterAll
