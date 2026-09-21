915d9fe1d49b65d074af0704c27e0a7737d4e7a8
draglide <darkzzangelzz@gmail.com>
2026-09-21 16:40:51 +0700

    test: advance Flyway baseline to v16

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
@@ -67,7 +67,7 @@ class AdministrativeAuthorizationPersistenceIntegrationTest {
                 new JdbcAdministrativeAuthorizationRepository(jdbc),
                 new IdentityGovernedActorStatusQuery(identities));
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("15");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("16");
     }
 
     @AfterAll
