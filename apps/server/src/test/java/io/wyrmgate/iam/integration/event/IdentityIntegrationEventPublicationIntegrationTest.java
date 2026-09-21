5c2edbad17f8d8ff4e1a96e1fea0d6f675f4523c
draglide <darkzzangelzz@gmail.com>
2026-09-21 15:35:09 +0700

    test: advance Flyway baseline to v14

--- apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
@@ -67,7 +67,7 @@ class IdentityIntegrationEventPublicationIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("13");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("14");
     }
 
     @AfterAll
