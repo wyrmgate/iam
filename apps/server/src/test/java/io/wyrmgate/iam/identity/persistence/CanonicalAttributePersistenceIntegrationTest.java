3f006b0b82ad53b5f3955cab97466b30e24ced08
draglide <darkzzangelzz@gmail.com>
2026-09-21 13:14:33 +0700

    test: expect Flyway migration version 12

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/CanonicalAttributePersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/CanonicalAttributePersistenceIntegrationTest.java
@@ -91,7 +91,7 @@ class CanonicalAttributePersistenceIntegrationTest {
         resolution = new CanonicalAttributeResolutionService(
                 attributes, sources, identities, facts, ids, transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("11");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("12");
     }
 
     @AfterAll
