35b90fe85c0d33908d5c8ad07a3c7d24338b91ab
draglide <darkzzangelzz@gmail.com>
2026-09-21 13:15:18 +0700

    test: expect Flyway migration version 12

--- apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
@@ -67,7 +67,7 @@ class IdentityIntegrationEventPublicationIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("11");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("12");
     }
 
     @AfterAll
