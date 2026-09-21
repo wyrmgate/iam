e4274443836503f72a5a8b98511f4b7389bd247a
draglide <darkzzangelzz@gmail.com>
2026-09-21 14:07:34 +0700

    test: expect Flyway migration version 13

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/IdentityPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/IdentityPersistenceIntegrationTest.java
@@ -64,7 +64,7 @@ class IdentityPersistenceIntegrationTest {
                 idGenerator,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("12");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("13");
     }
 
     @AfterAll
