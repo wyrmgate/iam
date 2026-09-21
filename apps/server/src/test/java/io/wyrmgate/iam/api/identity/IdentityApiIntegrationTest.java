fbbad0e2e4939a5acc146ec156ba333ef3b63cb8
draglide <darkzzangelzz@gmail.com>
2026-09-21 13:14:29 +0700

    test: expect Flyway migration version 12

--- apps/server/src/test/java/io/wyrmgate/iam/api/identity/IdentityApiIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/api/identity/IdentityApiIntegrationTest.java
@@ -109,7 +109,7 @@ class IdentityApiIntegrationTest {
                 new JdbcCanonicalAttributeReadRepository(jdbc),
                 evaluator);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("11");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("12");
     }
 
     @AfterAll
