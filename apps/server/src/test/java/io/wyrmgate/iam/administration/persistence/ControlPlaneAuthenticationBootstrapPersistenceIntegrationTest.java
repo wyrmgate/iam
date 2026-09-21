1eda6fe54305040e438a010c4c41b74c5e684d81
draglide <darkzzangelzz@gmail.com>
2026-09-21 11:03:43 +0700

    test: expect Flyway migration version 11

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
@@ -85,7 +85,7 @@ class ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("10");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("11");
     }
 
     @AfterAll
