49b3352ffd05ea58169de90e0a2dcb8edc03ac22
draglide <darkzzangelzz@gmail.com>
2026-09-21 16:41:07 +0700

    test: advance Flyway baseline to v16

--- apps/server/src/test/java/io/wyrmgate/iam/api/identity/IdentityApiIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/api/identity/IdentityApiIntegrationTest.java
@@ -109,7 +109,7 @@ class IdentityApiIntegrationTest {
                 new JdbcCanonicalAttributeReadRepository(jdbc),
                 evaluator);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("15");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("16");
     }
 
     @AfterAll
