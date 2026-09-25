a40d409e17aa09b2c3053e28f131216e7b56f0e1
draglide <darkzzangelzz@gmail.com>
2026-09-25 14:37:02 +0700

    test: expect effective access migration version

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
@@ -67,7 +67,7 @@ class AdministrativeAuthorizationPersistenceIntegrationTest {
                 new JdbcAdministrativeAuthorizationRepository(jdbc),
                 new IdentityGovernedActorStatusQuery(identities));
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("19");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("20");
     }
 
     @AfterAll
