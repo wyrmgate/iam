b63a7c0b7c63d943f66479b14d4606b1a64bea1a
draglide <darkzzangelzz@gmail.com>
2026-09-25 15:54:05 +0700

    test: expect grant provisioning migration version

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/SourceCorrelationPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/SourceCorrelationPersistenceIntegrationTest.java
@@ -76,7 +76,7 @@ class SourceCorrelationPersistenceIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("21");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("22");
     }
 
     @AfterAll
