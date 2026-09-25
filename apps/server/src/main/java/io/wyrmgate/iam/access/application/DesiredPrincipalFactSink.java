2c0e4a700ebd6d4aaaf4fafd6a3bfe865f8499bf
draglide <darkzzangelzz@gmail.com>
2026-09-25 18:20:41 +0700

    access: add desired principal revalidation trigger

--- apps/server/src/main/java/io/wyrmgate/iam/access/application/DesiredPrincipalFactSink.java
+++ apps/server/src/main/java/io/wyrmgate/iam/access/application/DesiredPrincipalFactSink.java
@@ -6,6 +6,9 @@ import io.wyrmgate.iam.platform.tenant.TenantContext;
 public interface DesiredPrincipalFactSink {
 
     String DESIRED_PRINCIPAL_CHANGED = "access.desired-principal-changed";
+    String DESIRED_PRINCIPAL_REVALIDATE = "access.desired-principal-revalidate";
 
     void changed(TenantContext tenant, DesiredPrincipalState state);
+
+    void revalidate(TenantContext tenant, DesiredPrincipalState state);
 }
