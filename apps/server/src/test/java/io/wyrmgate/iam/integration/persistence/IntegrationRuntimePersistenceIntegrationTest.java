1b1680e62a0f978d21b082605d13942b605fdca7
draglide <darkzzangelzz@gmail.com>
2026-09-25 15:54:42 +0700

    test: expect grant provisioning migration version

--- apps/server/src/test/java/io/wyrmgate/iam/integration/persistence/IntegrationRuntimePersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/integration/persistence/IntegrationRuntimePersistenceIntegrationTest.java
@@ -65,7 +65,7 @@ class IntegrationRuntimePersistenceIntegrationTest {
         repository = new JdbcIntegrationRuntimeRepository(
                 jdbc, json, ids, observedAccessFacts);
         transactions = new SpringTransactionExecutor(new DataSourceTransactionManager(dataSource));
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("21");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("22");
     }
 
     @AfterAll
