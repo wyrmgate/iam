4b62e87a1bea48fd82983d6ebfe7ecdceccf0654
draglide <darkzzangelzz@gmail.com>
2026-09-25 19:10:32 +0700

    test: advance Flyway checkpoint to v24

--- apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/CanonicalAttributePersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/identity/persistence/CanonicalAttributePersistenceIntegrationTest.java
@@ -91,7 +91,7 @@ class CanonicalAttributePersistenceIntegrationTest {
         resolution = new CanonicalAttributeResolutionService(
                 attributes, sources, identities, facts, ids, transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("22");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("24");
     }
 
     @AfterAll
