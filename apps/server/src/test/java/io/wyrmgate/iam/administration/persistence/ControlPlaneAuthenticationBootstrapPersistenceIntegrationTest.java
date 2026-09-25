3599b8525af32795962f9bdf1fb40426e2e390d9
draglide <darkzzangelzz@gmail.com>
2026-09-25 15:23:05 +0700

    test: expect desired state migration version

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
@@ -85,7 +85,7 @@ class ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("20");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("21");
     }
 
     @AfterAll
