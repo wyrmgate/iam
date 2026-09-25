e08d5e5dc8a659d16b99d6c63cd2cf7a73e67997
draglide <darkzzangelzz@gmail.com>
2026-09-25 11:34:12 +0700

    test: expect principal migration version

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest.java
@@ -85,7 +85,7 @@ class ControlPlaneAuthenticationBootstrapPersistenceIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("17");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("18");
     }
 
     @AfterAll
