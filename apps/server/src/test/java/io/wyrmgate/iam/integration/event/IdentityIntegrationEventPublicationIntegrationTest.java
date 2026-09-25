b6f7672aaea1969c3ac0329c71272a91f1e575a7
draglide <darkzzangelzz@gmail.com>
2026-09-25 11:34:32 +0700

    test: expect principal migration version

--- apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
@@ -67,7 +67,7 @@ class IdentityIntegrationEventPublicationIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("17");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("18");
     }
 
     @AfterAll
