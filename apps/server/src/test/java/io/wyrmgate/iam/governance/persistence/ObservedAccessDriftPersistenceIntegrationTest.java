d9c1a1f8bdc50f1b7bcb358db10f5458846c4973
draglide <darkzzangelzz@gmail.com>
2026-09-25 13:41:32 +0700

    test: expect access assignment migration version

--- apps/server/src/test/java/io/wyrmgate/iam/governance/persistence/ObservedAccessDriftPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/governance/persistence/ObservedAccessDriftPersistenceIntegrationTest.java
@@ -73,7 +73,7 @@ class ObservedAccessDriftPersistenceIntegrationTest {
         Flyway flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
         flyway.validate();
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("18");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("19");
 
         jdbc = new JdbcTemplate(dataSource);
         ids = new UuidV7Generator();
