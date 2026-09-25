964b78fd1a8a4a4326ca5ee6f5d292eebcce39db
draglide <darkzzangelzz@gmail.com>
2026-09-25 15:23:16 +0700

    test: expect desired state migration version

--- apps/server/src/test/java/io/wyrmgate/iam/api/integration/IntegrationAdminApiPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/api/integration/IntegrationAdminApiPersistenceIntegrationTest.java
@@ -112,7 +112,7 @@ class IntegrationAdminApiPersistenceIntegrationTest {
                 authorization, commands, integration,
                 mappingCommands, observationMappings,
                 idempotency, transactions);
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("20");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("21");
     }
 
     @AfterAll
