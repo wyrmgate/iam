f47d20ab51b6d757af6a2b3362d68bd7273a419f
draglide <darkzzangelzz@gmail.com>
2026-09-25 19:09:56 +0700

    test: advance Flyway checkpoint to v24

--- apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/administration/persistence/AdministrativeAuthorizationPersistenceIntegrationTest.java
@@ -67,7 +67,7 @@ class AdministrativeAuthorizationPersistenceIntegrationTest {
                 new JdbcAdministrativeAuthorizationRepository(jdbc),
                 new IdentityGovernedActorStatusQuery(identities));
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("22");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("24");
     }
 
     @AfterAll
