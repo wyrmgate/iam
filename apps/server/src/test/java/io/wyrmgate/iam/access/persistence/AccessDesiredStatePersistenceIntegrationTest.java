6a781218f93bbf5f60e7f6bed3ddbc7443f2d09c
draglide <darkzzangelzz@gmail.com>
2026-09-25 11:34:05 +0700

    test: expect principal migration version

--- apps/server/src/test/java/io/wyrmgate/iam/access/persistence/AccessDesiredStatePersistenceIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/access/persistence/AccessDesiredStatePersistenceIntegrationTest.java
@@ -44,7 +44,7 @@ class AccessDesiredStatePersistenceIntegrationTest {
         tenants = new JdbcTenantRepository(jdbc, ids);
         repository = new JdbcDesiredStateProjectionRepository(jdbc);
         query = new AccessDesiredStateQueryService(repository);
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("17");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("18");
     }
 
     @AfterAll
