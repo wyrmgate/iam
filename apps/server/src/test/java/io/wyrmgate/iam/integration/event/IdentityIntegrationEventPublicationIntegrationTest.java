85dc37c79a718d8007e5103d08fbaaa7f0565e7b
draglide <darkzzangelzz@gmail.com>
2026-09-26 14:48:26 +0700

    test: advance Flyway checkpoint to v25

--- apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
+++ apps/server/src/test/java/io/wyrmgate/iam/integration/event/IdentityIntegrationEventPublicationIntegrationTest.java
@@ -67,7 +67,7 @@ class IdentityIntegrationEventPublicationIntegrationTest {
                 ids,
                 transactions);
 
-        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("24");
+        assertThat(flyway.info().current().getVersion().getVersion()).isEqualTo("25");
     }
 
     @AfterAll
